3475b47b5bea86840c284dc1238432e79e9d7964
Adam Schubert <adam.schubert@sg1-game.net>
2017-07-06 17:37:45 +0200

    Work on Html snippet form

--- src/Dravencms/AdminModule/Components/HtmlSnippet/HtmlSnippetForm/HtmlSnippetForm.php
+++ src/Dravencms/AdminModule/Components/HtmlSnippet/HtmlSnippetForm/HtmlSnippetForm.php
@@ -46,94 +46,56 @@ class HtmlSnippetForm extends BaseControl
     /** @var EntityManager */
     private $entityManager;
 
-    /** @var ArticleRepository */
-    private $articleRepository;
-
-    /** @var StructureFileRepository */
-    private $structureFileRepository;
+    /** @var HtmlSnippetRepository */
+    private $htmlSnippetRepository;
 
     /** @var LocaleRepository */
     private $localeRepository;
 
-    /** @var TagRepository */
-    private $tagRepository;
+    /** @var HtmlSnippetTranslationRepository */
+    private $htmlSnippetTranslationRepository;
 
-    /** @var ArticleTranslationRepository */
-    private $articleTranslationRepository;
-
-    /** @var TagTranslationRepository */
-    private $tagTranslationRepository;
-
-    /** @var Group */
-    private $group;
-
-    /** @var File */
-    private $file;
-
-    /** @var Article|null */
-    private $article = null;
+    /** @var HtmlSnippet|null */
+    private $htmlSnippet = null;
 
     /** @var array */
     public $onSuccess = [];
 
     public function __construct(
-        Group $group,
         BaseFormFactory $baseFormFactory,
         EntityManager $entityManager,
-        ArticleRepository $articleRepository,
-        ArticleTranslationRepository $articleTranslationRepository,
-        TagRepository $tagRepository,
-        TagTranslationRepository $tagTranslationRepository,
-        StructureFileRepository $structureFileRepository,
+        HtmlSnippetRepository $htmlSnippetRepository,
+        HtmlSnippetTranslationRepository $htmlSnippetTranslationRepository,
         LocaleRepository $localeRepository,
-        File $file,
-        Article $article = null
+        HtmlSnippet $htmlSnippet = null
     ) {
         parent::__construct();
 
-        $this->group = $group;
-        $this->article = $article;
+        $this->htmlSnippet = $htmlSnippet;
 
         $this->baseFormFactory = $baseFormFactory;
         $this->entityManager = $entityManager;
-        $this->articleRepository = $articleRepository;
-        $this->articleTranslationRepository = $articleTranslationRepository;
-        $this->tagRepository = $tagRepository;
-        $this->structureFileRepository = $structureFileRepository;
-        $this->tagTranslationRepository = $tagTranslationRepository;
+        $this->htmlSnippetRepository = $htmlSnippetRepository;
+        $this->htmlSnippetTranslationRepository = $htmlSnippetTranslationRepository;
         $this->localeRepository = $localeRepository;
-        $this->file = $file;
 
-
-        if ($this->article) {
-            $tags = [];
-            foreach ($this->article->getTags() AS $tag) {
-                $tags[$tag->getId()] = $tag->getId();
-            }
+        if ($this->htmlSnippet) {
 
             $defaults = [
-                'structureFile' => ($this->article->getStructureFile() ? $this->article->getStructureFile()->getId() : null),
-                'position' => $this->article->getPosition(),
-                'identifier' => $this->article->getIdentifier(),
-                'isActive' => $this->article->isActive(),
-                'isShowName' => $this->article->isShowName(),
-                'isAutoDetectTags' => $this->article->isAutoDetectTags(),
-                'tags' => $tags
+                'identifier' => $this->htmlSnippet->getIdentifier(),
+                'isActive' => $this->htmlSnippet->isActive(),
+                'isShowName' => $this->htmlSnippet->isShowName()
             ];
 
-            foreach ($this->article->getTranslations() AS $translation)
+            foreach ($this->htmlSnippet->getTranslations() AS $translation)
             {
                 $defaults[$translation->getLocale()->getLanguageCode()]['name'] = $translation->getName();
-                $defaults[$translation->getLocale()->getLanguageCode()]['lead'] = $translation->getLead();
-                $defaults[$translation->getLocale()->getLanguageCode()]['subtitle'] = $translation->getSubtitle();
-                $defaults[$translation->getLocale()->getLanguageCode()]['perex'] = $translation->getPerex();
-                $defaults[$translation->getLocale()->getLanguageCode()]['text'] = $translation->getText();
+                $defaults[$translation->getLocale()->getLanguageCode()]['html'] = $translation->getHtml();
             }
         } else {
             $defaults = [
                 'isActive' => true,
-                'isShowName' => true,
-                'isAutoDetectTags' => true
+                'isShowName' => true
             ];
         }
 
@@ -151,34 +113,14 @@ class HtmlSnippetForm extends BaseControl
                 ->setRequired('Please enter article name.')
                 ->addRule(Form::MAX_LENGTH, 'Article name is too long.', 255);
 
-            $container->addText('lead')
-                ->setRequired(false)
-                ->addRule(Form::MAX_LENGTH, 'Article lead is too long.', 255);
-
-            $container->addText('subtitle')
-                ->setRequired(false)
-                ->addRule(Form::MAX_LENGTH, 'Article subtitle is too long.', 255);
-
-            $container->addTextArea('perex');
-
-            $container->addTextArea('text');
+            $container->addTextArea('html');
         }
 
         $form->addText('identifier')
             ->setRequired('Please fill in an identifier');
 
-
-        $form->addText('structureFile');
-
-        $form->addText('position')
-            ->setDisabled((is_null($this->article)));
-
-        $form->addMultiSelect('tags', null, $this->tagRepository->getPairs());
-
         $form->addCheckbox('isActive');
         $form->addCheckbox('isShowName');
-        $form->addCheckbox('isAutoDetectTags');
-
 
         $form->addSubmit('send');
 
@@ -193,13 +135,13 @@ class HtmlSnippetForm extends BaseControl
         $values = $form->getValues();
 
         foreach ($this->localeRepository->getActive() AS $activeLocale) {
-            if (!$this->articleTranslationRepository->isNameFree($values->{$activeLocale->getLanguageCode()}->name, $activeLocale, $this->group, $this->article)) {
+            if (!$this->htmlSnippetTranslationRepository->isNameFree($values->{$activeLocale->getLanguageCode()}->name, $activeLocale, $this->htmlSnippet)) {
                 $form->addError('Tento název je již zabrán.');
             }
         }
 
-        if (!$this->presenter->isAllowed('article', 'edit')) {
-            $form->addError('Nemáte oprávění editovat article.');
+        if (!$this->presenter->isAllowed('htmlSnippet', 'edit')) {
+            $form->addError('Nemáte oprávění editovat htmlSnippet.');
         }
     }
 
@@ -207,116 +149,39 @@ class HtmlSnippetForm extends BaseControl
     {
         $values = $form->getValues();
 
-        if ($values->isAutoDetectTags) {
-            foreach ($this->localeRepository->getActive() AS $activeLocale) {
-                foreach ($this->tagTranslationRepository->getAll($activeLocale) AS $tag) {
-                    if (strpos($values->{$activeLocale->getLanguageCode()}->text, $tag->getName()) !== false && !in_array($tag->getTag()->getId(), $values->tags)) {
-                        $values->tags[$tag->getTag()->getId()] = $tag->getTag()->getId();
-                    }
-                }
-            }
-        }
-
-        $tags = new ArrayCollection($this->tagRepository->getById($values->tags));
-
-        if ($values->structureFile) {
-            $structureFile = $this->structureFileRepository->getOneById($values->structureFile);
+        if ($this->htmlSnippet) {
+            $htmlSnippet = $this->htmlSnippet;
+            $htmlSnippet->setIdentifier($values->identifier);
+            $htmlSnippet->setIsActive($values->isActive);
+            $htmlSnippet->setIsShowName($values->isShowName);
         } else {
-            $structureFile = null;
+            $htmlSnippet = new HtmlSnippet($values->identifier, $values->isActive, $values->isShowName);
         }
 
-        if ($this->article) {
-            $article = $this->article;
-            $article->setIdentifier($values->identifier);
-            $article->setStructureFile($structureFile);
-            $article->setIsActive($values->isActive);
-            $article->setIsShowName($values->isShowName);
-            $article->setIsAutoDetectTags($values->isAutoDetectTags);
-            $article->setPosition($values->position);
-        } else {
-            $article = new Article($this->group, $values->isActive, $values->isShowName, $values->isAutoDetectTags, $structureFile);
-        }
-        $article->setTags($tags);
-
-        $this->entityManager->persist($article);
+        $this->entityManager->persist($htmlSnippet);
 
         $this->entityManager->flush();
 
         foreach ($this->localeRepository->getActive() AS $activeLocale) {
-            if ($articleTranslation = $this->articleTranslationRepository->getTranslation($article, $activeLocale))
+            if ($htmlSnippetTranslation = $this->htmlSnippetTranslationRepository->getTranslation($htmlSnippet, $activeLocale))
             {
-                $articleTranslation->setName($values->{$activeLocale->getLanguageCode()}->name);
-                $articleTranslation->setSubtitle($values->{$activeLocale->getLanguageCode()}->subtitle);
-                $articleTranslation->setLead($values->{$activeLocale->getLanguageCode()}->lead);
-                $articleTranslation->setText($values->{$activeLocale->getLanguageCode()}->text);
-                $articleTranslation->setPerex($values->{$activeLocale->getLanguageCode()}->perex);
+                $htmlSnippetTranslation->setName($values->{$activeLocale->getLanguageCode()}->name);
+                $htmlSnippetTranslation->setHtml($values->{$activeLocale->getLanguageCode()}->html);
             }
             else
             {
-                $articleTranslation = new ArticleTranslation(
-                    $article,
+                $htmlSnippetTranslation = new HtmlSnippetTranslation(
+                    $htmlSnippet,
                     $activeLocale,
                     $values->{$activeLocale->getLanguageCode()}->name,
-                    $values->{$activeLocale->getLanguageCode()}->subtitle,
-                    $values->{$activeLocale->getLanguageCode()}->lead,
-                    $this->cleanText($values->{$activeLocale->getLanguageCode()}->text),
-                    $values->{$activeLocale->getLanguageCode()}->perex
+                    $values->{$activeLocale->getLanguageCode()}->html
                 );
             }
-            $this->entityManager->persist($articleTranslation);
+            $this->entityManager->persist($htmlSnippetTranslation);
         }
         $this->entityManager->flush();
 
         $this->onSuccess();
-    }
-
-    /**
-     * @param $text
-     * @return string
-     */
-    private function cleanText($text)
-    {
-        $dom = new \DOMDocument('1.0', 'utf-8');
-        $text = mb_convert_encoding($text, 'HTML-ENTITIES', "UTF-8");
-        @$dom->loadHTML($text);
-
-        foreach ($dom->getElementsByTagName('div') as $node) {
-            $class = $node->getAttribute("class");
-            $classArray = explode(' ', $class);
-            $hasNote = false;
-            foreach ($classArray AS $k => $classItem) {
-                if (strpos($classItem, 'note-') !== false) {
-                    $hasNote = true;
-                    unset($classArray[$k]);
-                }
-            }
-
-            if ($hasNote) {
-                $node->removeAttribute("id");
-                $node->removeAttribute("contenteditable");
-            }
-
-            $node->setAttribute("class", implode(' ', $classArray));
-        }
-
-        foreach ($dom->getElementsByTagName('iframe') as $node) {
-            if ($node->hasAttribute("frameborder")) {
-                $node->removeAttribute("frameborder");
-            }
-        }
-
-        $unstyledElements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-        foreach ($unstyledElements AS $unstyledElement) {
-            foreach ($dom->getElementsByTagName($unstyledElement) as $node) {
-                if ($node->hasAttribute("style")) {
-                    $node->removeAttribute("style");
-                }
-            }
-        }
-
-        $xpath = new \DOMXPath($dom);
-        $body = $xpath->query('/html/body');
-        return html_entity_decode(strtr($dom->saveHTML($body->item(0)), array('<body>' => '', '</body>' => '')));
     }
 
     public function render()
@@ -324,7 +189,7 @@ class HtmlSnippetForm extends BaseControl
         $template = $this->template;
         $template->fileSelectorPath = $this->file->getFileSelectorPath();
         $template->activeLocales = $this->localeRepository->getActive();
-        $template->setFile(__DIR__ . '/ArticleForm.latte');
+        $template->setFile(__DIR__ . '/HtmlSnippetForm.latte');
         $template->render();
     }
 }
