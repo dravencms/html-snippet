89cec8218e539e30d404c1ad323c5aa0d5644c82
Adam Schubert <adam.schubert@sg1-game.net>
2022-03-04 16:09:45 +0100

    feat(Detail): Drop dependency on Tempnam and Cache

--- src/Dravencms/FrontModule/Components/HtmlSnippet/HtmlSnippet/Detail/Detail.php
+++ src/Dravencms/FrontModule/Components/HtmlSnippet/HtmlSnippet/Detail/Detail.php
@@ -6,10 +6,8 @@ use Dravencms\Components\BaseControl\BaseControl;
 use Dravencms\Locale\CurrentLocaleResolver;
 use Dravencms\Model\HtmlSnippet\Repository\HtmlSnippetRepository;
 use Dravencms\Model\HtmlSnippet\Repository\HtmlSnippetTranslationRepository;
-use Nette\Caching\Cache;
-use Nette\Caching\IStorage;
 use Dravencms\Structure\ICmsActionOption;
-use Salamek\Tempnam\Tempnam;
+use Latte\Loaders\StringLoader;
 
 class Detail extends BaseControl
 {
@@ -25,64 +23,41 @@ class Detail extends BaseControl
     /** @var HtmlSnippetTranslationRepository */
     private $htmlSnippetTranslationRepository;
 
-    /** @var Cache */
-    private $cache;
-
-    /** @var Tempnam */
-    private $tempnam;
-
     /**
      * Detail constructor.
      * @param ICmsActionOption $cmsActionOption
      * @param HtmlSnippetRepository $htmlSnippetRepository
      * @param HtmlSnippetTranslationRepository $htmlSnippetTranslationRepository
      * @param CurrentLocaleResolver $currentLocaleResolver
-     * @param IStorage $storage
-     * @param Tempnam $tempnam
      */
     public function __construct(
         ICmsActionOption $cmsActionOption,
         HtmlSnippetRepository $htmlSnippetRepository,
         HtmlSnippetTranslationRepository $htmlSnippetTranslationRepository,
-        CurrentLocaleResolver $currentLocaleResolver,
-        IStorage $storage,
-        Tempnam $tempnam
+        CurrentLocaleResolver $currentLocaleResolver
     )
     {
         $this->cmsActionOption = $cmsActionOption;
         $this->htmlSnippetRepository = $htmlSnippetRepository;
         $this->htmlSnippetTranslationRepository = $htmlSnippetTranslationRepository;
         $this->currentLocale = $currentLocaleResolver->getCurrentLocale();
-        $this->tempnam = $tempnam;
-        $this->cache = new Cache($storage, __CLASS__);
     }
 
     public function render(): void
     {
-        $template = $this->template;
         $htmlSnippet = $this->htmlSnippetRepository->getOneByIdAndActive($this->cmsActionOption->getParameter('id'));
         $htmlSnippetTranslation = $this->htmlSnippetTranslationRepository->getTranslation($htmlSnippet, $this->currentLocale);
 
+        $template = $this->template;
+        $template->getLatte()->setLoader(new StringLoader([
+            'html.snippet' => $htmlSnippetTranslation->getHtml(),
+            'html.snippet.container' => file_get_contents(__DIR__.'/detail.latte')
+        ]));
+       
         $template->htmlSnippet = $htmlSnippet;
         $template->htmlSnippetTranslation = $htmlSnippetTranslation;
 
-        $key = __CLASS__ . $htmlSnippetTranslation->getId();
-
-        $tempFile = $this->tempnam->load($key, $htmlSnippetTranslation->getUpdatedAt());
-        $invalidateLatteCache = false;   
-        if ($tempFile === null) {
-            $temp = file_get_contents(__DIR__ . '/detail.latte');
-
-            $temp = strtr($temp, ['<!--HTML-SNIPPET-->' => $htmlSnippetTranslation->getHtml()]);
-            $tempFile = $this->tempnam->save($key, $temp, $htmlSnippetTranslation->getUpdatedAt());
-            $invalidateLatteCache = true;
-        }
-
-        $template->setFile($tempFile);
-        if ($invalidateLatteCache) {
-            $latteTmpFile = $template->getLatte()->getCacheFile($template->getFile());
-            if (is_file($latteTmpFile)) unlink($latteTmpFile);
-        }
+        $template->setFile('html.snippet.container');
         $template->render();
     }
 }
